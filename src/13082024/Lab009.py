3581c9e7e1527bcb7c126f5b624acd90eeca3f02
Suneel <suneel.chegudi@gmail.com>
2024-08-14 06:08:34 +0530

    Lab on 13th of August

---
+++ src/13082024/Lab009.py
@@ -0,0 +1,4 @@
+table = int(input("Enter the table you want to display: \n"))
+for number in range(1,11):
+    print(f"{table}*{number}: {table*number}")
+
---
+++ src/13082024/Lab010.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter the first number: \n"))
+num2 = int(input("Enter the second number: \n"))
+
+print(f"Max value between num1 and num2 is : {max(num1,num2)}")
+print(f"Power of num1 to num2 is : {pow(num1,num2)}")
+print(f"Sum of num1 and num2 is : {(num1-num2)}")
+print(f"Subtraction of num1 and num2 is : {(num1-num2)}")
+print(f"Multiplication of num1 and num2 is : {(num1*num2)}")
+print(f"Division of num1 and num2 is : {(num1/num2)}")
