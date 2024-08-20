629ec3c6a6b3dca0d0453f36cd662ddc7ab15573
Suneel <suneel.chegudi@gmail.com>
2024-08-20 23:23:01 +0530

    Lab on 20th of August

--- src/20082024/Task08.py
+++ src/20082024/Task08.py
@@ -2,7 +2,7 @@ side1 = float(input("Enter the length of side#1:\n"))
 side2 = float(input("Enter the length of side#2:\n"))
 side3 = float(input("Enter the length of side#3:\n"))
 
-if side1 == side2 and side2 == side3 and side1 == side3:
+if side1 == side2 and side1 == side3:
     print ("Equilateral Triangle")
 elif side1 == side2 or side1 == side3 or side2 == side3:
     print("isosceles")
